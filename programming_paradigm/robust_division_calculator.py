275f7e37759d07b50a9fca467d1c376f9d18d4a5
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-07-12 21:29:09 +0300

    divisonCalc

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+        print(f"The result of the division is {result}")
+
+    except ZeroDivisionError:
+        print(f"Error: Cannot divide by zero.")
+    except ValueError:
+        print(f"Error: Please enter numeric values only.")
+    except TypeError:
+        print(f"Error: Please enter numeric values only.")

be740a845813f4c13e2d8cca7ee744825c852fe7
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-07-14 15:21:28 +0300

    folder with scripts

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,44 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out=False):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+
+    def check_out(self, title):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        else:
+            return False
+
+    def returning(self, title):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        else:
+            return False
+
+
+class Library:
+    def __init__(self):
+        _books = []
+        self.book = _books
+
+    def add_book(self, newBook):
+        self.book.append(newBook)
+
+    def list_available_books(self):
+        for book in self.book:
+            print(f"{book.title} by {book.author}")
+
+    def check_out_book(self, title):
+        for book in self.book:
+            if book.title == title:
+                return book.check_out()
+        return False
+
+    def return_book(self, title):
+        for book in self.book:
+            if book.title == title:
+                return book.returning()
+            return False
--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,6 +10,7 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         """Test the addition method."""
+        self.assertGreater(10, 3, "should be true")
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(5, 1), 6)
@@ -18,7 +19,7 @@ class TestSimpleCalculator(unittest.TestCase):
     # Remember to write additional test methods for subtract, multiply, and divide.
     def test_subtraction(self):
         """Test the addition method."""
-        self.assertEqual(self.calc.subtract(10, 1), 9)
+        self.assertEqual(self.calc.subtract(20,10),10)
         self.assertEqual(self.calc.subtract(-1, -7), 6)
 
     def test_multiplication(self):
@@ -29,7 +30,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_division(self):
         """Test the addition method."""
         self.assertEqual(self.calc.divide(50, 10), 5)
-        self.assertEqual(self.calc.divide(10, 0), "zero divided error")
+        self.assertEqual(self.calc.divide(10, 5), 2)
 
 
 if __name__ == "__main__":

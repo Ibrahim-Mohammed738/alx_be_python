fa8ee989499449e4edaca0b7843d73eebb7efe26
Ibrahim Mohammed <ibrahimsayed3220@gmail.com>
2024-07-20 04:01:43 +0300

    oop/ Inheritance and Composition

---
+++ oop/library_system.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for b in self.books:
+            if isinstance(b, Book):
+                print(f"Book: {b.title} by {b.author}")
+            elif isinstance(b, EBook):
+                print(f"EBook: {b.title} by {b.author}, File Size: {b.File_Size}KB")
+            elif isinstance(b, PrintBook):
+                print(f"PrintBook: {b.title} by {b.author}, Page Count: {b.page_count}")
